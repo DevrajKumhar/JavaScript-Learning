586f4d790c3fcef0a461315542c27ad128e5b9d8
DevrajKumhar <devrajkumhar90@gmail.com>
2024-09-10 05:59:30 +0000

    datatypes

---
+++ JavaScript/Basics/datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // ue all js code as newer version
+
+//alert(3 + 10) //we are usig nodejs, not browser
+
+console.log(10   
+     +           //code readabilidy should be high
+     20);
+
+console.log("Devraj");
+
+let name = "Devraj"  //string type
+let age = 20         //integer type
+let login = true     //boolean type
+
+
+
+// bigint
+// string => ""
+// boolean => true/false
+// number => 2 to the power 53
+// null => standalone value
+// undefined => value is not asigned
+// symbol => unique
+
+
+//object
+
+console.log(typeof undefined);  //output -> undefined
+console.log(typeof null);       //output -> object
+
+
