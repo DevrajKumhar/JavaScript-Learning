24b84d74311771eeafc53749ee10846078e2d506
DevrajKumhar <devrajkumhar90@gmail.com>
2024-09-10 06:34:19 +0000

    conversion opration

---
+++ JavaScript/Basics/conversionOpration.js
@@ -0,0 +1,33 @@
+let score = null         //33, 33abc, null, undefined, true, 
+
+//console.log(typeof score);
+//console.log(typeof(score) );
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+            // "33" => 33
+            // "33abc" => NaN(not a number)
+            // true => 1; false => 0
+            // null => 0
+            // undefined => NaN
+
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+            // 1 => true; 0 => false
+            // "" => false
+            // "Devraj" => true
+
+let SomeNumber = 22
+
+let stringNumber = String(SomeNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
