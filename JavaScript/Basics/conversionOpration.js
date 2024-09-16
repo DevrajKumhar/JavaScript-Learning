c8158e8ee71c638e95a91f7cb027d04b2fba6706
DevrajKumhar <devrajkumhar90@gmail.com>
2024-09-16 06:35:20 +0000

    conversionOperation

--- JavaScript/Basics/conversionOpration.js
+++ JavaScript/Basics/conversionOpration.js
@@ -28,12 +28,44 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let SomeNumber = 22
 
 let stringNumber = String(SomeNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 /* *************************************************** OPERATIONS ********************************************************** */
 
 let value = 3
 let negvalue = -value
-console.log(negvalue)
+// console.log(negvalue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**3);
+
+let str1 = "hello"
+let str2 = "Devraj"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3+4) * 5 % 3);
+
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100 
+gameCounter++;
+console.log(gameCounter);
 
