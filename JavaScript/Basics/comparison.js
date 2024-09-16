f44ae7ce0b24d9d32ad6884777ac97a4b5b3ef8d
DevrajKumhar <devrajkumhar90@gmail.com>
2024-09-16 17:39:29 +0000

    I learn comparison in JavaScript

---
+++ JavaScript/Basics/comparison.js
@@ -0,0 +1,11 @@
+/*
+console.log(2 > 1); //true
+console.log(2 >= 1); //true
+console.log(2 < 1); //false
+console.log(2 == 1); //false
+console.log(2 <= 1); //false
+*/
+console.log(null > 0);
+console.log(null >= 0);
+console.log(undefined > 0);
+console.log(undefined >= 0);
