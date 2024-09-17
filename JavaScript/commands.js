f5d2c395ce771777e1874c45cbf4079380faecf7
DevrajKumhar <devrajkumhar90@gmail.com>
2024-09-17 04:18:00 +0000

    commands

---
+++ JavaScript/commands.js
@@ -0,0 +1,9 @@
+// ls command -> To get a list of files and folders in your current directory
+// cd command -> To change directories, type cd followed by the name of the directory you want to enter then press return.
+              // For example, cd Desktop .
+
+// git add ->  adds a change in the working directory to the staging area.
+// git restore -> to restore files in the working directory or the staging area (index) to their last committed state.
+// git status -> tells the current state of the repository.
+// git commit -> Adding a Git commit message should look something like this:
+              // git commit -m “Add an anchor for the trial end sectionnn.”              
