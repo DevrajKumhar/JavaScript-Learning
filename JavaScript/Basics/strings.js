1add3dbd185622bc65795b5b058f9ef4c79954c0
DevrajKumhar <devrajkumhar90@gmail.com>
2024-09-17 16:51:01 +0000

    string properties

---
+++ JavaScript/Basics/strings.js
@@ -0,0 +1,37 @@
+const name = "Devraj"
+const repoCount = 50
+
+//console.log(name + repoCount + "value");
+
+
+// backticks -> (` `)
+//console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Devraj-Kumhar-shahapura')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(2)); //charAt(number) -> it is used to check the character at given position
+// console.log(gameName.indexOf('r')); // indexOf(character) -> it is used to check the position of given character
+
+const newString = gameName.substring(0,5)
+// console.log(newString);
+
+const anotherString = gameName.slice(-12,5)
+// console.log(anotherString);
+
+const newStringOne = "          helloDevraj     "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "http://devraj.com/kumhar%10kumar"
+
+// console.log(url);
+// console.log(url.replace('%10', '-'));
+// console.log(url.includes('kumar'));
+// console.log(gameName.split('-'));
+
