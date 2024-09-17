9af8263583acba01f0b539c689a7089494d2c5df
DevrajKumhar <devrajkumhar90@gmail.com>
2024-09-17 14:52:24 +0000

    summary of datatypes

--- JavaScript/Basics/comparison.js
+++ JavaScript/Basics/comparison.js
@@ -6,7 +6,13 @@ console.log(2 == 1); //false
 console.log(2 <= 1); //false
 */
 
-console.log(null > 0);
-console.log(null >= 0);
-console.log(undefined > 0);
-console.log(undefined >= 0);
+// console.log(null > 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined >= 0);
+
+// console.log("2" == 2);   //false
+// console.log("2" === 2)); //true
+
+// (===) -> checks whether its two operands are equal, returning a Boolean result.
---
+++ JavaScript/Basics/dataTypesSummary.js
@@ -0,0 +1,31 @@
+//  Primitive 
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id == anotherId);
+
+const bigNumber = 3213456876542n
+
+// Reference (Non Primitive)
+
+// Array, Objects, Functions 
+
+const heros = ["shaktiman", "ironman", "hulk"];
+let myObj = {
+    name: "devraj",
+    age: 21,
+}
+
+const myFunction = function(){
+    console.log("Hello Devraj");
+    
+}
